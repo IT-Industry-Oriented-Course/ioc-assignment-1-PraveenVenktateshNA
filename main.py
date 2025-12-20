be149dff7354de943cacc0fb35167c86a22a0fc0
PraveenVenktateshNA <napraveenvenkatesh@gmail.com>
2025-12-20 09:21:56 +0530

    Update main.py for refusal request

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from agent.agent import run_agent
 
 query = "Schedule a cardiology follow-up for patient Ravi Kumar next week and check insurance eligibility"
-
+#query = "Hello World"
 result = run_agent(query)
 print(result)
